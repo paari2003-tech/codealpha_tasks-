152793b70dfeb7278731da8a914fc9bd66bc86a9
Paari oviya <proppify@gmail.com>
2026-05-31 15:11:56 +0530

    Removed practice.py

--- Network_Packet_Tracer/practice.py
+++
@@ -1,9 +0,0 @@
-words = ["apple", "banana", "apple", "orange", "banana", "apple"]
-frequency = {}
-
-for word in words:
-    if word in frequency:
-        frequency[word] += 1
-    else:
-        frequency[word] = 1
-print(frequency)
